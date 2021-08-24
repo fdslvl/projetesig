c1ae495f20e2d10f83a01a5babe5f44512e87d05
elv-fabio <elv-fabio.dslvl@eduge.ch>
2021-08-24 22:22:51 +0200

    Incrementation et Decrementation
    incrémenter et décrémenter des produits dans le panier, le prix change en conséquence

--- leshop/apps/commande/admin.py
+++ leshop/apps/commande/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Commande, CommandeItem
+
 # Register your models here.
+
+admin.site.register(Commande)
+admin.site.register(CommandeItem)
--- leshop/apps/commande/models.py
+++ leshop/apps/commande/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+from apps.store.models import Produit
+
 # Create your models here.
+
+class Commande(models.Model):
+    premier_nom = models.CharField(max_length=100)
+    dernier_nom = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    adresse= models.CharField(max_length=100)
+    npa = models.CharField(max_length=100)
+    localite = models.CharField(max_length=100)
+
+    cree_le = models.DateTimeField(auto_now_add=True)
+
+    paye = models.BooleanField(default=False)
+    montant_paye = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return '%s' % self.premier_nom
+
+class CommandeItem(models.Model):
+    commande = models.ForeignKey(Commande, related_name='items', on_delete=models.CASCADE)
+    produit = models.ForeignKey(Produit, related_name='items', on_delete=models.DO_NOTHING)
+    prix = models.FloatField()
+    quantite = models.IntegerField(default=1)
+
+    def __str__(self):
+        return '%s' % self.id
