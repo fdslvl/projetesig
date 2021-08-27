3457ea24e1598aee90dea79e392a29137d972fd2
elv-fabio <elv-fabio.dslvl@eduge.ch>
2021-08-27 19:15:35 +0200

    Mises en forme
    Mises en forme des formulaires et tests avec images

--- leshop/apps/userprofile/forms.py
+++ leshop/apps/userprofile/forms.py
@@ -7,6 +7,16 @@ class FormulaireInscription(UserCreationForm):
     nom = forms.CharField(max_length=50, required=True)
     email= forms.EmailField(max_length=255, required=True)
 
+    def __init__(self, *args, **kwargs):
+        super(FormulaireInscription, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'input'
+        self.fields['email'].widget.attrs['class'] = 'input'
+        self.fields['password1'].widget.attrs['class'] = 'input'
+        self.fields['password2'].widget.attrs['class'] = 'input'
+        self.fields['prenom'].widget.attrs['class'] = 'input'
+        self.fields['nom'].widget.attrs['class'] = 'input'
+
 class Meta:
     model = User
     fields = ['username', 'pernom', 'nom', 'email', 'password1', 'password2']
